833b391b51bf2f09e94d0d18e2c2bef7ae283271
Rathnasingam Kesavaram <rathnasingam.kesav@hcl.com>
2023-09-21 10:55:28 +0530

    Add project files.

---
+++ GigHub/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using GigHub.Models;
+
+namespace GigHub.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
