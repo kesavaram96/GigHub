950727d51e5cab1c7c522cc252af6af9ccccd38a
Rathnasingam Kesavaram <rathnasingam.kesav@hcl.com>
2023-09-21 16:22:49 +0530

    GigCreate

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GigHub.Data;
+using GigHub.Models;
+using GigHub.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Security.Claims;
 
 namespace GigHub.Controllers
 {
+    
     public class GigsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+        [Authorize]
         public IActionResult Create()
         {
+            IEnumerable<Genre> genres=_context.Genres;
+            ViewBag.Genres = new SelectList(genres, "Id", "Name");
             return View();
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult Create(GigFormViewModel model) 
+        {
+            var artist = _context.Users.Single(u => u.Id == User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var genre = _context.Genres.Single(u => u.Id == model.Genre);
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", model.Date, model.Time)),
+                Genre = genre,
+                Venue = model.Venue,
+            };
+            _context.Add(gig);
+            _context.SaveChanges(); 
+            return RedirectToAction("Index","Home");
         }
     }
 }
---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GigHub.Models;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+
+        
+    }
+}
